dd9962f57e87e9229be187ffc46e4b9b7a5e037c
Serhii Kholin <xain@Serhiis-MacBook-Pro.local>
2016-06-28 12:45:30 +0300

    added functional tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -15,11 +15,32 @@ describe('Onix Bots Application', function() {
       expect(botList.count()).toBe(3);
 
       query.sendKeys('converter');
-      expect(phoneList.count()).toBe(2);
+      expect(botList.count()).toBe(2);
 
       query.clear();
       query.sendKeys('portal');
-      expect(phoneList.count()).toBe(1);
+      expect(botList.count()).toBe(1);
+    });
+
+    it('should be possible to control bots order via the drop-down menu', function() {
+        var queryField = element(by.model('$ctrl.query'));
+        var orderSelect = element(by.model('$ctrl.orderProp'));
+        var nameOption = orderSelect.element(by.css('option[value="name"]'));
+        var botNameColumn = element.all(by.repeater('bot in $ctrl.bots').column('bot.name'));
+
+        function getNames() {
+            return botNameColumn.map(function(elem){
+                return elem.getText();
+            });
+        }
+
+        queryField.sendKeys('Portal'); // Let's narrow the dataset to make the assertions shorter
+
+        expect(getNames()).toEqual([
+            'Portal Cinema Kirovohrad for Telegram'
+        ]);
+
+
     });
 
   });
