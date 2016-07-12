01b8464fdd9a81a1c76d4134969da340d15532ae
Serg Xain <xainSE@gmail.com>
2016-07-12 22:39:56 +0300

    add test for home and detailed page

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -8,6 +8,8 @@ describe('Onix Bots Application', function() {
       browser.get('index.php');
     });
 
+
+    // Test #1
     it('should filter the bot list as a user types into the search box', function() {
       var botList = element.all(by.repeater('bot in $ctrl.bots'));
       var query = element(by.model('$ctrl.query'));
@@ -22,6 +24,7 @@ describe('Onix Bots Application', function() {
       expect(botList.count()).toBe(1);
     });
 
+    // Test #2
     it('should be possible to control bots order via the drop-down menu', function() {
         var queryField = element(by.model('$ctrl.query'));
         var orderSelect = element(by.model('$ctrl.orderProp'));
@@ -39,8 +42,31 @@ describe('Onix Bots Application', function() {
         expect(getNames()).toEqual([
             'Portal Cinema Kirovohrad for Telegram'
         ]);
+    });
+
+    // Test #3
+    it('should redirect index.php to index.php#!/bots', function(){
+      browser.get('index.php');
+      expect(browser.getLocationAbsUrl()).toBe('/bots')
+    });
+  });
 
 
+  describe('View: bot list', function(){
+    beforeEach(function(){
+      browser.get('index.php#!/bots')
+    })
+  });
+
+
+  describe('View: Bot details', function() {
+
+    beforeEach(function() {
+      browser.get('index.pho#!/bot/converter-units-fb');
+    });
+
+    it('should display placeholder page with `botId`', function() {
+      expect(element(by.binding('$ctrl.botId')).getText()).toBe('converter-units-fb');
     });
 
   });
