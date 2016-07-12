e11d9e8bde91486b9f9370d04e6cab4f7a30a74d
Serg Xain <xainSE@gmail.com>
2016-07-12 23:42:40 +0300

    add bot detail page

--- app/bot-detail/bot-detail.component.js
+++ app/bot-detail/bot-detail.component.js
@@ -4,9 +4,14 @@ angular.
   module('botDetail').
   component('botDetail', {
     template: 'TBD: Detail view for <span>{{$ctrl.botId}}</span>',
-    controller: ['$routeParams',
-      function BotDetailController($routeParams) {
-        this.botId = $routeParams.botId;
+    templateUrl: 'bot-detail/bot-detail.template.html',
+    controller: ['$http', '$routeParams',
+      function BotDetailController($http, $routeParams) {
+        var self = this;
+
+        $http.get('bots/' + $routeParams.botId + '.json').then(function(responce){
+          self.bot = responce.data;
+        });
       }
     ]
   });
